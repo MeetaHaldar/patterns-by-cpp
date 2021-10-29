10e31101c0caa925b0cbe450ae0dc9e460c90d1b
meeta haldar <meetahaldar1001@gmail.com>
2021-10-29 20:07:10 +0530

    pattern-11 completed

---
+++ pattern-11.cpp
@@ -0,0 +1,41 @@
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int row=0; row<2*n;row++)
+    {
+        if(row<=n)
+        {
+            for(int col=0 ; col<row;col++)
+            {
+               cout<<"* ";
+            }
+            cout<<endl;
+
+            }
+           
+            else{
+         for(int col=1 ; col<=((2*n)-row);col++)
+            {
+                cout<<"* ";
+            }   
+
+              cout<<endl;
+        }
+        
+    }
+}
